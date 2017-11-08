2690370a0885bd0b3073d8663f4e78b499ff64cf
zhiyong-ye <yezhiyong@foreveross.com>
2017-11-08 17:02:49 +0800

    Setting Eureka-server application.yml and start

--- eureka-server/src/main/java/com/springcloud/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/springcloud/eurekaserver/EurekaServerApplication.java
@@ -2,7 +2,12 @@ package com.springcloud.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * EnableEurekaServer eureka-server服务注册中心
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
