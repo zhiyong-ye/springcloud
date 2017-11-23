5996325cde157465b2632871bbe50a7157e2db7c
zhiyong-ye <yezhiyong@foreveross.com>
2017-11-23 17:36:21 +0800

    the springcloud config-server project

---
+++ config-server/src/main/java/com/springcloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @EnableConfigServer	开启服务配置中心服务端
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
